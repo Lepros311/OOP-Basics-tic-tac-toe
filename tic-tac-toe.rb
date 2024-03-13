4715b0015a95de46d419e0ad01276a16e9405c19
Lepros311 <65778034+Lepros311@users.noreply.github.com>
2024-03-12 20:18:54 -0400

    Add classes, draw board, and get player selection.

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,5 +1,57 @@
-puts "   |   |   "
-puts "---|---|---"
-puts "   |   |   "
-puts "---|---|---"
-puts "   |   |   "
+# classes:
+# player - 2 objects: player1 and player2
+# 	method(s) - select_square
+# piece - 2 object: X's and O's
+# board - 1 object: the board
+# 	method(s) - draw_board, check_winner
+
+class Player
+  def initialize
+
+  end
+
+  def select_square
+    puts "Select an empty square (1-9): "
+    selection = gets.chomp
+  end
+end
+
+class Piece
+  def initialize
+
+  end
+end
+
+class Board
+  attr_accessor :row1, :row2, :row3
+  attr_reader :line
+
+  def initialize
+    @row1 = [" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "]
+    @line = ["---|---|---"]
+    @row2 = [" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "]
+    @row3 = [" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "]
+  end
+
+  def draw_board
+    puts row1.join('')
+    puts line
+    puts row2.join('')
+    puts line
+    puts row3.join('')
+  end
+
+  def sqaure_open?
+
+  end
+
+  def check_winner
+
+  end
+end
+
+our_board = Board.new
+our_board.draw_board
+player1 = Player.new
+player2 = Player.new
+player1.select_square
