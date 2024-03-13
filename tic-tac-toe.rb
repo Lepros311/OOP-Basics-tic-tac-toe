cfec1f2420ac8640dcc5ee83c9ec42706fef170d
Lepros311 <65778034+Lepros311@users.noreply.github.com>
2024-03-13 11:17:55 -0400

    Add ability to successfully place piece on the board.

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,10 +1,3 @@
-# classes:
-# player - 2 objects: player1 and player2
-# 	method(s) - select_square
-# piece - 2 object: X's and O's
-# board - 1 object: the board
-# 	method(s) - draw_board, check_winner
-
 class Player
   def initialize
 
@@ -12,21 +5,27 @@ class Player
 
   def select_square
     puts "Select an empty square (1-9): "
-    selection = gets.chomp
+    selected_square = gets.chomp
   end
 end
 
 class Piece
-  def initialize
+  attr_accessor :player_piece
 
+  def initialize(player_piece)
+    @player_piece = player_piece
+  end
+
+  def to_s
+    player_piece
   end
 end
 
 class Board
   attr_accessor :row1, :row2, :row3
-  attr_reader :line
+  attr_reader :line, :player_piece, :selected_square
 
-  def initialize
+  def initialize(player_piece)
     @row1 = [" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "]
     @line = ["---|---|---"]
     @row2 = [" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "]
@@ -41,8 +40,33 @@ class Board
     puts row3.join('')
   end
 
-  def sqaure_open?
+  def square_open?
 
+  end
+
+  def add_piece(player_piece, selected_square)
+    case selected_square
+    when "1"
+      row1[1] = player_piece
+    when "2"
+      row1[5] = player_piece
+    when "3"
+      row1[9] = player_piece
+    when "4"
+      row2[1] = player_piece
+    when "5"
+      row2[5] = player_piece
+    when "6"
+      row2[9] = player_piece
+    when "7"
+      row3[1] = player_piece
+    when "8"
+      row3[5] = player_piece
+    when "9"
+      row3[9] = player_piece
+    else
+      "Invalid selection. Please choose an open square (1-9): "
+    end
   end
 
   def check_winner
@@ -50,8 +74,14 @@ class Board
   end
 end
 
-our_board = Board.new
-our_board.draw_board
+
 player1 = Player.new
 player2 = Player.new
-player1.select_square
+
+puts "Player 1, do you want to be X or O?"
+player1piece = Piece.new(gets.chomp)
+selected_square = player1.select_square
+
+our_board = Board.new(player1piece)
+our_board.add_piece(player1piece, selected_square)
+our_board.draw_board
