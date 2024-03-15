18334e9e07d88ac04cad10389ea39e87bb8a614d
Lepros311 <65778034+Lepros311@users.noreply.github.com>
2024-03-15 12:03:43 -0400

    Resolve error after selecting first square.

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -5,10 +5,24 @@ class Player
     @name = name
   end
 
-  def select_square
-    puts "#{name}, select an empty square (1-9): "
-    selected_square = gets.chomp
+  def select_square(board)
+    selected_square = nil
+
+    loop do
+      puts "#{name}, select an empty square (1-9): "
+      selected_square = gets.chomp
+
+      if !selected_square.match?(/[1-9]/)
+        puts "You must enter a number between 1-9!"
+      elsif !board.square_open?(selected_square)
+        puts "That square is taken!"
+      else
+        break
+      end
+    end
+    selected_square
   end
+
 end
 
 class Piece
@@ -24,7 +38,7 @@ class Piece
 end
 
 class Board
-  attr_accessor :row1, :row2, :row3
+  attr_accessor :row1, :row2, :row3, :selected_squares
   attr_reader :line, :player_piece
 
   def initialize(player_piece)
@@ -32,6 +46,7 @@ class Board
     @line = ["---|---|---"]
     @row2 = [" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "]
     @row3 = [" ", " ", " ", "|", " ", " ", " ", "|", " ", " ", " "]
+    @selected_squares = []
   end
 
   def draw_board
@@ -42,8 +57,8 @@ class Board
     puts row3.join('')
   end
 
-  def square_open?
-
+  def square_open?(selected_square)
+    !selected_squares.include?(selected_square)
   end
 
   def add_piece(player_piece, selected_square)
@@ -66,19 +81,18 @@ class Board
       row3[5] = player_piece
     when "9"
       row3[9] = player_piece
-    else
-      "Invalid selection. Please choose an open square (1-9): "
     end
+    selected_squares.push(selected_square)
   end
 
-  def player2turn(player2, player_piece)
-    selected_square = player2.select_square
+  def player2turn(player2, player_piece, board)
+    selected_square = player2.select_square(self)
     self.add_piece(player_piece, selected_square)
     self.draw_board
   end
 
-  def player1turn(player1, player_piece)
-    selected_square = player1.select_square
+  def player1turn(player1, player_piece, board)
+    selected_square = player1.select_square(self)
     self.add_piece(player_piece, selected_square)
     self.draw_board
   end
@@ -104,17 +118,21 @@ class Board
 
  def play_game(player1, player2, player1piece, player2piece)
    until game_over?(player1piece) || game_over?(player2piece)
-        self.player2turn(player2, player2piece)
-        break if game_over?(player2piece)  # Check if player2 has won before player1's turn
-        self.player1turn(player1, player1piece)
+        self.player2turn(player2, player2piece, self)
+        break if game_over?(player2piece)
+        break if [@row1[1], @row1[5], @row1[9],
+        @row2[1], @row2[5], @row2[9],
+        @row3[1], @row3[5], @row3[9]].all? { |square| square != " " }
+        self.player1turn(player1, player1piece, self)
+        break if [@row1[1], @row1[5], @row1[9],
+        @row2[1], @row2[5], @row2[9],
+        @row3[1], @row3[5], @row3[9]].all? { |square| square != " " }
    end
    if game_over?(player1piece)
     puts "#{player1.name} wins!"
    elsif game_over?(player2piece)
     puts "#{player2.name} wins!"
-   elsif [@row1[1], @row1[5], @row1[9],
-    @row2[1], @row2[5], @row2[9],
-    @row3[1], @row3[5], @row3[9]].all? { |square| square != " " }
+   else
     puts "It's a draw!"
    end
   end
@@ -126,12 +144,24 @@ player1 = Player.new(gets.chomp)
 puts "Player 2, what's your name?"
 player2 = Player.new(gets.chomp)
 
-puts "#{player1.name}, do you want to be X or O?"
-player1piece = Piece.new(gets.chomp)
-player2piece = Piece.new(player1piece.player_piece == "X" ? "O" : "X")
-selected_square = player1.select_square
+player1piece = nil
+loop do
+  puts "#{player1.name}, do you want to be X or O?"
+  player1piece = Piece.new(gets.chomp)
+
+  if player1piece.player_piece == 'X' || player1piece.player_piece == "O"
+    break
+  else
+    puts "Invalid option. Enter X or O: "
+  end
+end
 
 our_board = Board.new(player1piece)
+
+player2piece = Piece.new(player1piece.player_piece == "X" ? "O" : "X")
+selected_square = player1.select_square(our_board)
+
+
 our_board.add_piece(player1piece, selected_square)
 our_board.draw_board
 
