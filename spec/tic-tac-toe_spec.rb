4c0f666b974d39f03eba90c5362fa79cfad4921f
Lepros311 <65778034+Lepros311@users.noreply.github.com>
2024-04-17 18:18:04 -0400

    Add testing files

---
+++ spec/tic-tac-toe_spec.rb
@@ -0,0 +1,12 @@
+require './tic-tac-toe'
+
+describe Board do
+  describe "#game_over?" do
+    subject(:game_end) { Board.new }
+    it "return true when any winning combo"
+    expect(game_end.game_over?).to eq(true)
+  end
+end
+
+#So you need a Board to run the method on and the expectation, right?
+# Start with writing out the expectation. Then, get yourself a Board. Then, make it have a particular state (for example, X-O-nil nil-nil-nil O-X-nil or however you store the board state).
