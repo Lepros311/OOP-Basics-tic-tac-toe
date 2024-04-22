40ec7e699b424ad5799f65ae2dbd1d888a2de7af
Lepros311 <65778034+Lepros311@users.noreply.github.com>
2024-04-22 10:51:15 -0400

    Add more tests

--- spec/tic-tac-toe_spec.rb
+++ spec/tic-tac-toe_spec.rb
@@ -2,12 +2,51 @@ require_relative '../tic-tac-toe'
 
 describe Board do
   describe "#game_over?" do
-    subject(:game_end) { Board.new }
-    it "return true when any winning combo" do
-      expect(game_end.game_over?).to eq(true)
+    subject(:game_end) { Board.new('X') }
+    it "returns true with a winning combo" do
+      game_end.add_piece('X', '1')
+      game_end.add_piece('X', '2')
+      game_end.add_piece('X', '3')
+      expect(game_end.game_over?('X')).to eq(true)
+    end
+
+    it "returns false with a non-winning combo" do
+      game_end.add_piece('X', '1')
+      game_end.add_piece('X', '2')
+      game_end.add_piece('X', '5')
+      expect(game_end.game_over?('X')).to eq(false)
     end
   end
 end
 
-#So you need a Board to run the method on and the expectation, right?
-# Start with writing out the expectation. Then, get yourself a Board. Then, make it have a particular state (for example, X-O-nil nil-nil-nil O-X-nil or however you store the board state).
+describe Player do
+  describe "#select_square" do
+    let(:board) { instance_double(Board) }
+    subject(:player) { described_class.new("Player 1") }
+
+    context "when player selects an empty square" do
+      before do
+        allow(player).to receive(:puts)
+        allow(player).to receive(:gets).and_return("5\n") # Stubbing user input for square 5
+        allow(board).to receive(:square_open?).with("5").and_return(true) # Stubbing board method
+      end
+      it "returns the selected square" do
+        expect(player.select_square(board)).to eq("5")
+      end
+    end
+
+    context "when player selects a square that is already taken" do
+      before do
+        allow(player).to receive(:puts)
+        allow(player).to receive(:gets).and_return("3\n") # Stubbing user input for square 3
+        allow(board).to receive(:square_open?).with("3").and_return(false) # Stubbing board method
+      end
+      it "prompts the player to select another square" do
+        expect(player).to receive(:puts).with("That square is taken!")
+        player.select_square(board)
+      end
+    end
+
+
+  end
+end
--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -139,31 +139,31 @@ class Board
 
 end
 
-puts "Player 1, what's your name?"
-player1 = Player.new(gets.chomp)
-puts "Player 2, what's your name?"
-player2 = Player.new(gets.chomp)
+# puts "Player 1, what's your name?"
+# player1 = Player.new(gets.chomp)
+# puts "Player 2, what's your name?"
+# player2 = Player.new(gets.chomp)
 
-player1piece = nil
-loop do
-  puts "#{player1.name}, do you want to be X or O?"
-  player1piece = Piece.new(gets.chomp)
+# player1piece = nil
+# loop do
+#   puts "#{player1.name}, do you want to be X or O?"
+#   player1piece = Piece.new(gets.chomp)
 
-  if player1piece.player_piece == 'X' || player1piece.player_piece == "O"
-    break
-  else
-    puts "Invalid option. Enter X or O: "
-  end
-end
+#   if player1piece.player_piece == 'X' || player1piece.player_piece == "O"
+#     break
+#   else
+#     puts "Invalid option. Enter X or O: "
+#   end
+# end
 
-our_board = Board.new(player1piece)
+# our_board = Board.new(player1piece)
 
-player2piece = Piece.new(player1piece.player_piece == "X" ? "O" : "X")
-our_board.draw_board
-selected_square = player1.select_square(our_board)
+# player2piece = Piece.new(player1piece.player_piece == "X" ? "O" : "X")
+# our_board.draw_board
+# selected_square = player1.select_square(our_board)
 
 
-our_board.add_piece(player1piece, selected_square)
-our_board.draw_board
+# our_board.add_piece(player1piece, selected_square)
+# our_board.draw_board
 
-our_board.play_game(player1, player2, player1piece, player2piece)
+# our_board.play_game(player1, player2, player1piece, player2piece)
